4259c68068c97d8a7dbce2acb731b11d28921af2
Alina Skripets <skripetsalina92@gmail.com>
2015-10-05 03:25:38 +0300

    change checkbox,radio

--- js/nav_menu.js
+++ js/nav_menu.js
@@ -1,9 +1,23 @@
 $(function() {
-	$('#menuToggle').click(function() {
+/*	$('#menuToggle').click(function() {
 	    if($(this).is(':checked')) {
 	        $('.admin_menu').animate({left: "-700px"});
 	    } else {
 	       $('.admin_menu').animate({left: "0px"});
+	    }
+	});*/
+	$('#menuToggle').click(function() {
+	    if($(this).is(':checked')) {
+	    	$('.driver').css('display','block');
+	    	$('.driver').css('position','absolute');
+	    	$('.driver').css('left','0');
+	    	$('.driver').css('top','0');
+	        $('.driver').animate({left: "-700px"});
+	    } else {
+
+	       $('.driver').animate({left: "0px"});
+	       	$('.driver').css('display','table-cell');
+	    	$('.driver').css('position','static');
 	    }
 	});
 });
