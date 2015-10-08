8f785146461e79ca53cc48051ff5254d63b68724
Alina Skripets <skripetsalina92@gmail.com>
2015-10-08 05:27:09 +0300

    Change border-radius in pagination for pages with 2 and more numbers

--- js/input.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('input').each(function() {
-    	var isDisabled = $(this).prop('disabled');
-    	if (isDisabled) {
-        	$(this).attr("placeholder", "Disabled");
-    	}
-	});
-});
--- js/page_change.js
+++ js/page_change.js
@@ -1,15 +1,7 @@
 $(function() {
-    $('.one_page').click(function() {
-        $('.not_always_visible_number').css('color','#fff');
-        $('.always_visible_number').css('color','#d7dbdd');
-        $(this).children('a').css('color','#939393 !important');
-    });
- // var array = document.getElementsByName("page");
- // var l= array.length;
- // var x = document.getElementsByName("page")[2].innerHTML;
- // console.log(x);
- // for (i=0, i>l, i++) {
-	// document.getElementsByName("page")[i].innerHTML=9;
- // }
-
+	$( ".page" ).each(function( index ) {
+		if ($( this ).text()>9) {
+			$( this ).removeClass( "page" ).addClass( "page_2_numbers" );
+		}
+	});
 });
